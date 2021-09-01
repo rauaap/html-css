97d8f7d4f2dbbe802c23b47cdef292f461393364
Aapo Raukovaara <s1aara00@students.osao.fi>
2021-09-01 12:21:53 +0300

    ...

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-var pageNames = ['Ansioluettelo', 'CV', 'Stuff']
-var links = ['ansioluettelo.html', 'cv.html', 'stuff.html']
+var pageNames = ['Ansioluettelo', 'CV', 'Stuff', 'Morestuff']
+var links = ['ansioluettelo.html', 'cv.html', 'stuff.html', 'morestuff.html']
 window.addEventListener('load', genMenu)
 
 function genMenu() {
     var navbar = document.getElementById('navbar')
     var list = document.createElement('ul')
+    list.className = 'navigation'
     for ( var i = 0; i < links.length; i++ ) {
         var li = document.createElement('li')
         li.className = 'menu-item top'
