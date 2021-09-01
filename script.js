8a774a87b7c0df296508a178991e6bd6a5c5038e
apoleon <apoleon@apoleon-pc.com>
2021-09-01 19:26:39 +0300

    major overhaul

--- script.js
+++ script.js
@@ -5,39 +5,37 @@ window.addEventListener('load', genMenu)
 window.addEventListener('load', genFooter)
 
 function createHomeButton() {
-    var li = document.createElement('li')
-    li.className = 'menu-item navbar-item'
+    var div = document.createElement('div')
+    div.className = 'menu-item navbar-item'
     var a = document.createElement('a')
+    a.href = '/'
+    a.className = 'home-link'
     var img = document.createElement('img')
     img.className = 'menu-icon'
     img.src = '/img/home.png'
     a.appendChild(img)
-    li.appendChild(a)
-    return li
+    div.appendChild(a)
+    return div
 }
 
 function genMenu() {
     var navbar = document.getElementById('navbar')
-    navbar.className = 'page-wide-bar'
-    var list = document.createElement('ul')
-    list.className = 'navigation'
-    list.id = 'topMenu'
-    list.appendChild( createHomeButton() )
+    navbar.className = 'page-wide-bar top fixed'
+    navbar.appendChild( createHomeButton() )
     for ( var i = 0; i < links.length; i++ ) {
-        var li = document.createElement('li')
-        li.className = 'menu-item navbar-item'
+        var div = document.createElement('div')
+        div.className = 'menu-item navbar-item'
         var a = document.createElement('a')
         a.href = '/pages/' + links[i]
         a.innerHTML = pageNames[i]
-        li.appendChild(a)
-        list.appendChild(li)
+        div.appendChild(a)
+        navbar.appendChild(div)
     }
-    navbar.appendChild(list)
 }
 
 function genFooter() {
     var footer = document.getElementById('footer')
-    footer.className = 'page-wide-bar bottom'
+    footer.className = 'page-wide-bar bottom fixed'
     for ( i of footerText ) {
         var textElem = document.createElement('div')
         textElem.className = 'footer-text'
