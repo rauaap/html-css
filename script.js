dc80859b96323a89cb7019b9021479f7ababb5d2
apoleon <you@example.com>
2021-09-02 17:27:21 +0300

    link in footer

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const pageNames = ['Ansioluettelo', 'CV', 'Stuff', 'Morestuff']
 const links = ['ansioluettelo.html', 'cv.html', 'stuff.html', 'morestuff.html']
-const footerText = ['Aapo Raukovaara', 's1aara00@students.osao.fi', 'test']
+const footerText = ['Aapo Raukovaara', 's1aara00@students.osao.fi',
+                    ['Github', 'https://github.com/aaprau/html_css']]
 var homeButtonHighlighted = new Image()
 homeButtonHighlighted.src = '/img/home2.png'
 window.addEventListener('load', genMenu)
@@ -10,10 +11,10 @@ text.innerHTML = loremIpsum
 
 function createHomeButton() {
     var div = document.createElement('div')
-    div.className = 'menu-item navbar-item'
+    div.className = 'menu-item navbar-item centered-flex'
     var a = document.createElement('a')
     a.href = '/'
-    a.className = 'home-link'
+    a.className = 'home-link centered-flex'
     var img = document.createElement('img')
     img.id = 'homeButton'
     img.addEventListener('mouseover', highlightHomeButton)
@@ -40,7 +41,7 @@ function genMenu() {
     navbar.appendChild( createHomeButton() )
     for ( var i = 0; i < links.length; i++ ) {
         var div = document.createElement('div')
-        div.className = 'menu-item navbar-item'
+        div.className = 'navbar-item edges-text centered-flex'
         var a = document.createElement('a')
         a.href = '/pages/' + links[i]
         a.innerHTML = pageNames[i]
@@ -54,8 +55,22 @@ function genFooter() {
     footer.className = 'page-wide-bar bottom fixed'
     for ( i of footerText ) {
         var textElem = document.createElement('div')
-        textElem.className = 'footer-text'
+        textElem.className = 'footer-text edges-text centered-flex'
+        if ( Array.isArray(i) ) {
+            insertLink(textElem, i)
+        }
+        else {
         textElem.innerHTML = i
+        }
         footer.appendChild(textElem)
     }
+}
+
+function insertLink(elem, linkArray) {
+    var text = linkArray[0]
+    var link = linkArray[1]
+    var a = document.createElement('a')
+    a.href = link
+    a.innerHTML = text
+    elem.appendChild(a)
 }
